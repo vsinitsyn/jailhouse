d6b8b4e34111fe3a20336e29c57b4ac6e68216e8
Valentine Sinitsyn <valentine.sinitsyn@gmail.com>
2014-06-28 21:54:40 +0600

    x86: Fix handle a guest setting EFER
    Guests are not allowed to clear EFER.SVME, so we unconditionally add
    this flag to any guest EFER update.

--- hypervisor/arch/x86/svm.c
+++ hypervisor/arch/x86/svm.c
@@ -868,11 +868,11 @@ static bool svm_handle_msr_write(struct registers *guest_regs, struct per_cpu *c
 		x2apic_handle_write(guest_regs);
 		return true;
 	}
-	if (guest_regs->rcx == MSR_EFER &&
-	    !(guest_regs->rax & EFER_SVME)) {
-		panic_printk("Ignoring guest attempt to clear SVME\n");
-		/* TODO: Maybe simple inject #GP or similar into the guest? */
-		return false;
+	if (guest_regs->rcx == MSR_EFER) {
+		/* Never let a guest to disable SVME; see APMv2, Sect. 3.1.7 */
+		vmcb->efer = (guest_regs->rax & 0xffffffff) |
+			(guest_regs->rdx << 32) | EFER_SVME;
+		return true;
 	}
 
 	panic_printk("FATAL: Unhandled MSR write: %x\n",
