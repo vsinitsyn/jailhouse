b4c20adc2bbc4098b7ce559efce81af5d11b6778
Valentine Sinitsyn <valentine.sinitsyn@gmail.com>
2014-08-10 00:21:55 +0600

    x86: Switchable guest code access for mmio_parse()
    Refactored mmio_parse() and related to use link-time resolved, rather
    than hardcoded static function, to access guest memory. This way,
    vendor-specific code can provide accelerated implementations if needed.
    
    Code access function usage was also changed: it now returns pointer to the
    first byte available and accepts the number of bytes to map (or otherwise
    make available for the hypervisor). The function can adjust this value (it is
    now done to save an unnecessary page table walk), as described in <asm/vcpu.h>.

--- hypervisor/arch/x86/mmio.c
+++ hypervisor/arch/x86/mmio.c
@@ -2,9 +2,11 @@
  * Jailhouse, a Linux-based partitioning hypervisor
  *
  * Copyright (c) Siemens AG, 2013
+ * Copyright (c) Valentine Sinitsyn, 2014
  *
  * Authors:
  *  Jan Kiszka <jan.kiszka@siemens.com>
+ *  Valentine Sinitsyn <valentine.sinitsyn@gmail.com>
  *
  * This work is licensed under the terms of the GNU GPL, version 2.  See
  * the COPYING file in the top-level directory.
@@ -13,6 +15,8 @@
 #include <jailhouse/mmio.h>
 #include <jailhouse/paging.h>
 #include <jailhouse/printk.h>
+
+#include <asm/vcpu.h>
 
 union opcode {
 	u8 raw;
@@ -32,35 +36,28 @@ union opcode {
 	} __attribute__((packed)) sib;
 };
 
-/* If current_page is non-NULL, pc must have been increased exactly by 1. */
-static u8 *map_code_page(struct per_cpu *cpu_data,
-			 const struct guest_paging_structures *pg_structs,
-			 unsigned long pc, u8 *current_page)
-{
-	/* If page offset is 0, previous pc was pointing to a different page,
-	 * and we have to map a new one now. */
-	if (current_page && ((pc & ~PAGE_MASK) != 0))
-		return current_page;
-	return page_map_get_guest_page(cpu_data, pg_structs, pc,
-				       PAGE_READONLY_FLAGS);
-}
-
 struct mmio_access mmio_parse(struct per_cpu *cpu_data, unsigned long pc,
 			      const struct guest_paging_structures *pg_structs,
 			      bool is_write)
 {
 	struct mmio_access access = { .inst_len = 0 };
+	unsigned int remaining = 15, size = 0;
 	bool has_rex_r = false;
+	const u8 *inst = NULL;
 	bool does_write;
 	union opcode op[3];
-	u8 *page = NULL;
 
 restart:
-	page = map_code_page(cpu_data, pg_structs, pc, page);
-	if (!page)
-		goto error_nopage;
+	if (!size) {
+		size = remaining;
+		inst = vcpu_get_inst_bytes(cpu_data, pg_structs, pc, &size);
+		if (!inst)
+			goto error_noinst;
+		remaining -= size;
+		pc += size;
+	}
 
-	op[0].raw = page[pc & PAGE_OFFS_MASK];
+	op[0].raw = *inst;
 	if (op[0].rex.code == X86_REX_CODE) {
 		/* REX.W is simply over-read since it is only affects the
 		 * memory address in our supported modes which we get from the
@@ -70,7 +67,7 @@ restart:
 		if (op[0].rex.x)
 			goto error_unsupported;
 
-		pc++;
+		inst++; size--;
 		access.inst_len++;
 		goto restart;
 	}
@@ -89,12 +86,17 @@ restart:
 		goto error_unsupported;
 	}
 
-	pc++;
-	page = map_code_page(cpu_data, pg_structs, pc, page);
-	if (!page)
-		goto error_nopage;
+	inst++; size--;
+	if (!size) {
+		size = remaining;
+		inst = vcpu_get_inst_bytes(cpu_data, pg_structs, pc, &size);
+		if (!inst)
+			goto error_noinst;
+		remaining -= size;
+		pc += size;
+	}
 
-	op[1].raw = page[pc & PAGE_OFFS_MASK];
+	op[1].raw = *inst;
 	switch (op[1].modrm.mod) {
 	case 0:
 		if (op[1].modrm.rm == 5) /* 32-bit displacement */
@@ -102,12 +104,17 @@ restart:
 		else if (op[1].modrm.rm != 4) /* no SIB */
 			break;
 
-		pc++;
-		page = map_code_page(cpu_data, pg_structs, pc, page);
-		if (!page)
-			goto error_nopage;
+		inst++; size--;
+		if (!size) {
+			size = remaining;
+			inst = vcpu_get_inst_bytes(cpu_data, pg_structs, pc, &size);
+			if (!inst)
+				goto error_noinst;
+			remaining -= size;
+			pc += size;
+		}
 
-		op[2].raw = page[pc & PAGE_OFFS_MASK];
+		op[2].raw = *inst;
 		if (op[2].sib.ss != 0 || op[2].sib.index != 4 ||
 		    op[2].sib.base != 5)
 			goto error_unsupported;
@@ -134,8 +141,8 @@ restart:
 
 	return access;
 
-error_nopage:
-	panic_printk("FATAL: unable to map MMIO instruction page\n");
+error_noinst:
+	panic_printk("FATAL: unable to get MMIO instruction\n");
 	goto error;
 
 error_unsupported:
