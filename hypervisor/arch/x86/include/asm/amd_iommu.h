649ab17c3862e2e468b5683de57a462686d6b1c8
Valentine Sinitsyn <valentine.sinitsyn@gmail.com>
2014-09-09 17:38:32 +0600

    x86: Link-time decision support for AMD-V code
    This commit fixes AMD-V code broken by the previous one.
    Also fixed copyright headers.

--- hypervisor/arch/x86/include/asm/amd_iommu.h
+++ hypervisor/arch/x86/include/asm/amd_iommu.h
@@ -1,7 +1,7 @@
 /*
  * Jailhouse, a Linux-based partitioning hypervisor
  *
- * Copyright (c) Siemens AG, 2013
+ * Copyright (c) Valentine Sinitsyn, 2014
  *
  * Authors:
  *  Valentine Sinitsyn <valentine.sinitsyn@gmail.com>
@@ -13,38 +13,10 @@
 #ifndef _JAILHOUSE_ASM_AMD_IOMMU_H
 #define _JAILHOUSE_ASM_AMD_IOMMU_H
 
-#include <jailhouse/pci.h>
-
-#include <asm/cell.h>
-
-#include <jailhouse/cell-config.h>
-
-int amd_iommu_init(void);
-
-int amd_iommu_cell_init(struct cell *cell);
-int amd_iommu_map_memory_region(struct cell *cell,
-			  const struct jailhouse_memory *mem);
-int amd_iommu_unmap_memory_region(struct cell *cell,
-			    const struct jailhouse_memory *mem);
-int amd_iommu_add_pci_device(struct cell *cell, struct pci_device *device);
-void amd_iommu_remove_pci_device(struct pci_device *device);
-
-struct apic_irq_message
-amd_iommu_get_remapped_root_int(unsigned int iommu, u16 device_id,
-		unsigned int vector, unsigned int remap_index);
-int amd_iommu_map_interrupt(struct cell *cell, u16 device_id, unsigned int vector,
-		struct apic_irq_message irq_msg);
-
-void amd_iommu_cell_exit(struct cell *cell);
-
-void amd_iommu_config_commit(struct cell *cell_added_removed);
-
-void amd_iommu_shutdown(void);
-
-void amd_iommu_check_pending_faults(struct per_cpu *cpu_data);
-
-int amd_iommu_mmio_access_handler(bool is_write, u64 addr, u32 *value);
-
-bool amd_iommu_cell_ir_emulation(struct cell *cell);
+/*
+ * There is nothing in this file yet, since AMD IOMMU support is missing.
+ * It is assumed that AMD IOMMU-specific constants and definitions will
+ * appear here someday.
+ */
 
 #endif
